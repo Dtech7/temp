d3c06b61edd7252c870354f45383803349cdba2f
Neil D Felix <neild.felix@gmail.com>
2017-10-25 13:55:35 -0400

    Add files via upload

---
+++ hmlssChldrn.py
@@ -0,0 +1,19 @@
+#Name: Neil Felix
+#Date: 10/25
+#Info: ask user for file name and output name; creates graph using data for file;
+#       saves graph as the output name given.
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dtIn = input("Enter file name: ")
+dtOut = input("Enter Output file name: ")
+data = pd.read_csv(dtIn, skiprows=5)
+data['Fraction'] = pop['Total Children in Shelter']/pop['Total Individuals in Shelter']
+
+data.plot(x = "Date of Census", y = "Fraction")
+#plt.show()
+
+fig = plt.gcf()
+fig.savefig(dtOut)
+
